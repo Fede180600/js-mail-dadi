583a0786adc5f006b708914a3e1ff3985a2a2ea9
Fede180600 <fede180600@libero.it>
2022-04-12 15:13:03 +0200

    user email prompt

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,10 @@
-alert("hello")
+//Chiedo all'utente la sua email 
+const userEmail = prompt("Inserisci la tua email");
+console.log(userEmail);
+//Creo la mia array di emails
+
+//Confronto la email dell'utente con quelle disponibili nella mia array
+    
+    //Se l'email dell'utente corrisponde con una della mia array allora può eseguire l'accesso 
+
+    //Se non corrisponde stampare un messaggio sull'esito del controllo
