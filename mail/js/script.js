017694492b2fc215739bdd392bf1bfbf9120eaff
Fede180600 <fede180600@libero.it>
2022-04-12 15:30:24 +0200

    email cycle done

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,11 +1,18 @@
-//Chiedo all'utente la sua email 
-const userEmail = prompt("Inserisci la tua email");
-console.log(userEmail);
 //Creo la mia array di emails
 const myEmails = ["fede180600@libero.it", "pinco.pallo@gmail.it", "panco.pinco@yahoo.it", "mio.cugggino@gmail.it"];
 console.log(myEmails);
-//Confronto la email dell'utente con quelle disponibili nella mia array
-    
+//Chiedo all'utente la sua email 
+const userEmail = prompt("Inserisci la tua email");
+console.log('Email utente', userEmail);
+//Confronto la email dell'utente con quelle disponibili nella mia array (ciclo)
+let emailFound = false;
+for (let i = 0; i < userEmail.length; i++) {
+    const thisEmail = myEmails[i];
     //Se l'email dell'utente corrisponde con una della mia array allora può eseguire l'accesso 
-
-    //Se non corrisponde stampare un messaggio sull'esito del controllo
+    if (thisEmail === userEmail) {
+        emailFound = true;
+    }
+    console.log(userEmail, 'Può accedere?', emailFound);
+}
+console.log(emailFound);
+//Se non corrisponde stampare un messaggio sull'esito del controllo
