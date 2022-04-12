f7ccd40819611a0cd03b9d608647f9e5baf9bdfb
Fede180600 <fede180600@libero.it>
2022-04-12 15:43:46 +0200

    dadi ex pesudo code

---
+++ dadi/js/script.js
@@ -0,0 +1,7 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//Genero un numero random per l'utente 
+
+//Genero un numero random per il computer
+
+//Confronto i due numeri: se userNumber > computerNumber --> Winner: user; altrimenti winner: computer.
