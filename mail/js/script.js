a00c879b2cced5554f2dc894ac2c4e3527a25720
Fede180600 <fede180600@libero.it>
2022-04-12 15:13:33 +0200

    my emails array

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,7 +2,8 @@
 const userEmail = prompt("Inserisci la tua email");
 console.log(userEmail);
 //Creo la mia array di emails
-
+const myEmails = ["fede180600@libero.it", "pinco.pallo@gmail.it", "panco.pinco@yahoo.it", "mio.cugggino@gmail.it"];
+console.log(myEmails);
 //Confronto la email dell'utente con quelle disponibili nella mia array
     
     //Se l'email dell'utente corrisponde con una della mia array allora può eseguire l'accesso 
